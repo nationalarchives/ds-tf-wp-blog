b55086534826a09120f722823ba832dd6503375f
domingobishop <domingobishop@gmail.com>
2020-11-26 14:43:36 +0000

    update create config vars

--- app/scripts/userdata.sh
+++ app/scripts/userdata.sh
@@ -30,6 +30,8 @@ sudo echo "# file: /etc/httpd/conf.d/wordpress.conf
 # Create WP config file
 cd /var/www/html
 /usr/local/bin/wp config create --dbhost=${db_host} --dbname=${db_name} --dbuser=${db_user} --dbpass=${db_pass} --allow-root --extra-php <<PHP
+define('WP_SITEURL', 'http://${domain}');
+define('WP_HOME', 'http://${domain}');
 define( 'TNA_CLOUD', false );
 define( 'WP_MEMORY_LIMIT', '256M' );
 define( 'WP_MAX_MEMORY_LIMIT', '2048M' );
