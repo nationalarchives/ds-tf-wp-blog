e14b78e13891ecc38f286a5a154718c9d12a4088
Chris Bishop <domingobishop@users.noreply.github.com>
2021-11-16 17:22:16 +0000

    refactor IAM, EFS and launch config

--- app/lc.tf
+++ app/lc.tf
@@ -26,23 +26,5 @@ data "template_file" "ec2_userdata" {
     vars = {
         mount_target       = aws_efs_file_system.wp_efs.dns_name
         mount_dir          = var.efs_mount_dir
-        db_host            = "db.${var.service}wp.${var.environment}.local"
-        db_name            = var.wp_db_name
-        db_user            = var.wp_db_username
-        db_pass            = var.wp_db_password
-        service            = var.service
-        environment        = var.environment
-        domain             = var.wp_domain_name
-        cdn_bucket_name    = var.cdn_bucket_name
-        cdn_aws_region     = var.cdn_aws_region
-        cdn_cloudfront_url = var.cdn_cloudfront_url
-        cdn_dir            = var.cdn_dir
-        ses_user           = var.ses_username
-        ses_pass           = var.ses_password
-        ses_host           = var.ses_host
-        ses_port           = var.ses_port
-        ses_secure         = var.ses_secure
-        ses_from_email     = var.ses_from_email
-        ses_from_name      = var.ses_from_name
     }
 }
