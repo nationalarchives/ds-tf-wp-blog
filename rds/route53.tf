4976a37d7d9240177ff5b3b81d27e3e1317c3f27
AdamBurnettNA <afburnett@ITs-MacBook-Pro.local>
2022-05-12 14:13:23 +0100

    Duplicate RDS

--- rds/route53.tf
+++ rds/route53.tf
@@ -7,3 +7,14 @@ resource "aws_route53_record" "db" {
         aws_db_instance.wp_db_main.address
     ]
 }
+
+resource "aws_route53_record" "db_temp" {
+    zone_id = var.route53_local_zone_id
+    name    = "db_temp.${var.service}wp.${var.environment}.local"
+    type    = "CNAME"
+    ttl     = "300"
+    records = [
+        aws_db_instance.wp_db_main_10_6.address
+    ]
+}
+
